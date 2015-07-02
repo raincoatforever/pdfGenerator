4f4e709a91c0d6800eba9c35da1d83f34247dddf
raincoatforever <akshirsa@usc.edu>
2015-07-01 20:50:13 -0700

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ var server = app.listen(process.env.PORT || 3000, function () {
 
   var host = server.address().address;
   var port = server.address().port;
-
+  console.log('template chya aai cha path-', mu.root);
   console.log('Example app listening at http://%s:%s', host, port);
 
 });
