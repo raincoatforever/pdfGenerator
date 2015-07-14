906839e02c1ee2cf3f0dd18b5ed0daf5b2dd5089
Harsh <shahmharsh@gmail.com>
2015-07-13 23:32:44 -0700

    Added Employee Weekly Timecard Report
    - Things remaining:
    a) Report still needs to show total number of hours clocked by each employee
    b) Adding title and beautification

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ var app = express();
 var conversion = require("phantom-html-to-pdf")();
 
 //Constants
-var REPORT_TYPE_EMPLOYEE_DISPATCH_A = "employeeWeeklyDispatch";
 var DISPATCH_BOARD_REPORT = "DispatchBoardReport";
+var REPORT_TYPE_EMPLOYEE_WEEKLY_DISPATCH = "employeeWeeklyDispatch";
+var REPORT_TYPE_EMPLOYEE_WEEKLY_TIMECARD = "employeeWeeklyTimecard";
 var TABLE_HEADER_NAME = "Name";
 var TABLE_HEADER_TRADE = "Trade";
 var TABLE_HEADER_EMPLOYEE_ID = "Employee ID";
@@ -32,7 +33,7 @@ var server = app.listen(process.env.PORT || 3000, function () {
 });
 
 app.get('/pussy', function (req, res) {
-  //var jsonPostBody= jsonObjForConversion.toString();
+
   var jsonPostBody = dispatchBoardTestingString.toString();
   console.log(jsonPostBody);
   createHTMLFromJSON(jsonPostBody.toString(), res);
@@ -44,21 +45,13 @@ app.get('/healthcheck', function (req, res) {
 });
 
 app.post('/pdfGenerate', function(req, res){
-    //var htmlPostBody = req.body;
     var jsonPostBody="";
     req.on('data', function(chunk) {
-      console.log("Received body data:");
-      console.log(chunk.toString());
-      jsonPostBody = jsonPostBody + chunk;
+        jsonPostBody = jsonPostBody + chunk;
     });
     
     req.on('end', function() {
-        
-        //console.log(jsonPostBody);
-        createHTMLFromJSON(jsonPostBody.toString(), res);
-        
-        
-          
+        createHTMLFromJSON(jsonPostBody.toString(), res);  
     });
 });
 
@@ -69,12 +62,14 @@ function createHTMLFromJSON(jsonData, res) {
 
     var htmlBody;
     switch(reportType.toLowerCase()) {
-        case REPORT_TYPE_EMPLOYEE_DISPATCH_A.toLowerCase():
+        case REPORT_TYPE_EMPLOYEE_WEEKLY_DISPATCH.toLowerCase():
             GenerateReportEmployeeWeeklyDispatch(jsonObj, res);
             break;
-
         case DISPATCH_BOARD_REPORT.toLowerCase():
             GenerateDispatchBoardReport(jsonObj, res);
+            break;
+        case REPORT_TYPE_EMPLOYEE_WEEKLY_TIMECARD.toLowerCase():
+            GenerateReportEmployeeWeeklyTimecard(jsonObj, res);
             break;
     }
 }
@@ -84,7 +79,7 @@ function GenerateDispatchBoardReport(jsonObj, res) {
     console.log(JSON.stringify(jsonObj));
     console.log(jsonObj.rdata.data);
 
-    for( var i=0; i < jsonObj.rdata.data.length; i++){
+    for( var i=0; i < jsonObj.rdata.data.length; i++) {
 
         var dataNode = jsonObj.rdata.data[i];
         console.log(JSON.stringify(dataNode.site));
@@ -106,13 +101,7 @@ function GenerateDispatchBoardReport(jsonObj, res) {
                 return employee;
             });
         };
-
     }
-
-    console.log("*******************");
-    //console.log(jsonObj.rdata.data.employeesArray(jsonObj.rdata.data[0].employees[0]));
-    console.log("*******************");
-
 
     var renderedHtml = "";
         mu.compileAndRender('DispatchBoardReport.html', jsonObj.rdata)
@@ -130,50 +119,165 @@ function GenerateDispatchBoardReport(jsonObj, res) {
                 });
                 //res.send(renderedHtml);
             });
+}
 
+function getJobHours(job) {
+    var totalHours = 0
+    var hours;
+    hours = job.hours.sat.ot;
+    if (isNumeric(hours)) {
+        totalHours += Number(hours);
+    }
+    hours = job.hours.sat.dt;
+    if (isNumeric(hours)) {
+        totalHours += Number(hours);
+    }
+    hours = job.hours.sun.dt;
+    if (isNumeric(hours)) {
+        totalHours += Number(hours);
+    }
+    hours = job.hours.mon.st;
+    if (isNumeric(hours)) {
+        totalHours += Number(hours);
+    }
+    hours = job.hours.mon.ot;
+    if (isNumeric(hours)) {
+        totalHours += Number(hours);
+    }
+    hours = job.hours.mon.dt;
+    if (isNumeric(hours)) {
+        totalHours += Number(hours);
+    }
+    hours = job.hours.tue.st;
+    if (isNumeric(hours)) {
+        totalHours += Number(hours);
+    }
+    hours = job.hours.tue.ot;
+    if (isNumeric(hours)) {
+        totalHours += Number(hours);
+    }
+    hours = job.hours.tue.dt;
+    if (isNumeric(hours)) {
+        totalHours += Number(hours);
+    }
+    hours = job.hours.wed.st;
+    if (isNumeric(hours)) {
+        totalHours += Number(hours);
+    }
+    hours = job.hours.wed.ot;
+    if (isNumeric(hours)) {
+        totalHours += Number(hours);
+    }
+    hours = job.hours.wed.dt;
+    if (isNumeric(hours)) {
+        totalHours += Number(hours);
+    }
+    hours = job.hours.thu.st;
+    if (isNumeric(hours)) {
+        totalHours += Number(hours);
+    }
+    hours = job.hours.thu.ot;
+    if (isNumeric(hours)) {
+        totalHours += Number(hours);
+    }
+    hours = job.hours.thu.dt;
+    if (isNumeric(hours)) {
+        totalHours += Number(hours);
+    }
+    hours = job.hours.fri.st;
+    if (isNumeric(hours)) {
+        totalHours += Number(hours);
+    }
+    hours = job.hours.fri.ot;
+    if (isNumeric(hours)) {
+        totalHours += Number(hours);
+    }
+    hours = job.hours.fri.dt;
+    if (isNumeric(hours)) {
+        totalHours += Number(hours);
+    }
+
+    return totalHours;
+}
+
+function isNumeric(num) {
+    return !isNaN(num);
+}
+
+function GenerateReportEmployeeWeeklyTimecard(jsonObj, res) {
+    for (var i = 0; i < jsonObj.rdata.data.employees.length; i++) {
+        var totalHours = 0;
+        var employee = jsonObj.rdata.data.employees[i];
+        employee.noOfJobs = employee.jobs.length;
+
+        var firstJob = employee.jobs.splice(0, 1);
+        employee.firstJob = firstJob[0];
+        employee.firstJob.jobHours = getJobHours(employee.firstJob);
+        totalHours += Number(employee.firstJob.jobHours);
+        
+        for (var j = 0; j < employee.noOfJobs - 1; j++) {
+            employee.jobs[j].jobHours = getJobHours(employee.jobs[j]);
+            totalHours += Number(employee.jobs[j].jobHours);
+        }
+
+        employee.totalHours = totalHours;
+    }
+
+    var renderedHtml = "";
+    mu.compileAndRender('EmployeeWeeklyTimecard.html', jsonObj.rdata)
+        .on('data', function (renderedData) {
+            renderedHtml += renderedData.toString();
+        })
+        .on('end', function () {
+            //console.log(renderedHtml);
+            conversion({ html: renderedHtml, paperSize: {margin:"0.5cm", orientation:"landscape", format:"A3"}}, function(err, pdf) {
+                console.log(pdf.numberOfPages);
+                res.setHeader("Content-Type", "application/pdf");
+                pdf.stream.pipe(res);
+            });
+        }
+    );
 }
 
 function GenerateReportEmployeeWeeklyDispatch(jsonObj, res) {
 
     jsonObj.rdata.header_color = "#6495ed";
     // convert array to map-array
-        jsonObj.rdata.data.datesArray = function() {
-            return this.dates.map(function (date) {
-                return {src: date};
-            });
-        };
+    jsonObj.rdata.data.datesArray = function() {
+        return this.dates.map(function (date) {
+            return {src: date};
+        });
+    };
 
-        // add color according to trade
-        jsonObj.rdata.data.employeesArray = function() {
-            return this.employees.map(function (employee) {
-                var color = "#ffffff";
-                if (employee.trade.toLowerCase() == "Carpenter".toLowerCase()) {
-                    color = "#66ffff";
-                } else if (employee.trade.toLowerCase() == "Mason".toLowerCase()) {
-                    color = "#ffff00";
-                } else if (employee.trade.toLowerCase() == "Laborer".toLowerCase()) {
-                    color = "#66ff66";
-                }
+    // add color according to trade
+    jsonObj.rdata.data.employeesArray = function() {
+        return this.employees.map(function (employee) {
+            var color = "#ffffff";
+            if (employee.trade.toLowerCase() == "Carpenter".toLowerCase()) {
+                color = "#66ffff";
+            } else if (employee.trade.toLowerCase() == "Mason".toLowerCase()) {
+                color = "#ffff00";
+            } else if (employee.trade.toLowerCase() == "Laborer".toLowerCase()) {
+                color = "#66ff66";
+            }
                 
-                employee.color = color;
-                return employee;
-            });
-        };
+            employee.color = color;
+            return employee;
+        });
+    };
 
-        var renderedHtml = "";
-        mu.compileAndRender('EmployeeWeeklyDispatch.html', jsonObj.rdata)
-            .on('data', function (renderedData) {
-                renderedHtml += renderedData.toString();
-            })
-            .on('end', function (){
-                //console.log(renderedHtml.toString());
-                //res.contentType('text/html');
-                console.log(renderedHtml);
-                conversion({ html: renderedHtml }, function(err, pdf) {
-                    console.log(pdf.numberOfPages);
-                    res.setHeader("Content-Type", "application/pdf");
-                    pdf.stream.pipe(res);
-                });
-                //res.send(renderedHtml);
+    var renderedHtml = "";
+    mu.compileAndRender('EmployeeWeeklyDispatch.html', jsonObj.rdata)
+        .on('data', function (renderedData) {
+            renderedHtml += renderedData.toString();
+        })
+        .on('end', function () {
+            //console.log(renderedHtml);
+            conversion({ html: renderedHtml }, function(err, pdf) {
+                console.log(pdf.numberOfPages);
+                res.setHeader("Content-Type", "application/pdf");
+                pdf.stream.pipe(res);
             });
+        }
+    );
 }
